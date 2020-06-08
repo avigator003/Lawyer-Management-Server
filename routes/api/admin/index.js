b7927e52da8389bb696ee71231f950144b29bf74
Anas M.I <thirdessential@gmail.com>
2020-06-08 22:42:46 +0530

    bug fix

--- routes/api/admin/index.js
+++ routes/api/admin/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const controller = require('./admin.controller')
 
 
-router.post("/showall", controller.showAll)
+router.get("/showall", controller.showAll)
 router.get("/block/:id", controller.blockuser)
 router.get("/unblock/:id", controller.unblockuser)
 module.exports = router
