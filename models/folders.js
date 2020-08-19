5ee13c0960346ec03253edf6ef74bc0dce1fd294
Anas M.I <anas3rde@gmail.com>
2020-08-19 19:07:04 +0530

    bug fix

--- models/folders.js
+++ models/folders.js
@@ -8,7 +8,7 @@ const Folders = new Schema({
     name:String,
     documents:[{type:Schema.Types.ObjectId, ref:"Documents"}],
     userId:{type:Schema.Types.ObjectId, ref:"User"},
-    folder:[{type:Schema.Types.ObjectId, ref:"Folder"}],
+    folder:[{type:Schema.Types.ObjectId, ref:"Folders"}],
 type:String
 },schemaOptions)
 
