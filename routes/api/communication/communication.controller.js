d0eaa23ce17fd84e568715e73ee87042086dbbda
Anas M.I <anas3rde@gmail.com>
2020-09-02 20:50:10 +0530

    sms api

--- routes/api/communication/communication.controller.js
+++ routes/api/communication/communication.controller.js
@@ -172,6 +172,61 @@ exports.editCommunication = (req, res) => {
 
     // Communication.find({userId:req.params.id, from:req.params.contact}).populate("from matter to")
     //     .then(data => {
+            res.status(200).json({status: true, message:" fetched"})
+
+//         }).catch(error => {
+//         res.status(200).json({status: false, message:error})
+
+//         })
+}
+
+
+exports.sendSms = (req, res) => {
+    
+
+    // var transporter = nodemailer.createTransport({
+    //     service: 'gmail',
+    //     auth: {
+    //       user: 'anas3rde@gmail.com',
+    //       pass: '8123342590'
+    //     }
+    //   });
+    
+    //   var mailOptions = {
+    //     from: 'FromName <registeredMail@xxx.com>',
+    //     to: 'registeredMail@xyz.com', // list of receivers
+    //     subject: 'Amazon SES Template TesT', // Subject line
+    //     html: <p>Mail message</p> // html body
+    // };
+
+        // var url = "https://distracted-rosalind-719c41.netlify.app" +'/verified/?token='+user._id;
+      
+    //   var userEmail = user.emailAddress;
+    //   var emailText = 'Please click on the link below to verify your Account';
+    // //   emailText += '<p><a href="'+url+'">click here</a>';
+    //   var mailOptions = {
+    //     from: 'anas3rde@gmail.com',
+    //     to: req.body.to,
+    //     subject: req.body.subject,
+    //     html: req.body.text
+    //   };
+      
+    //   transporter.sendMail(mailOptions, function(error, info){
+    //     if (error) {
+    //       console.log(error);
+    //     //   res.json({ 'success': false, 'message': error });
+    //     } 
+          res.json({ 'success': true, 'message': 'Sms sent ' })
+        
+    //   });
+
+
+
+
+
+
+    // Communication.find({userId:req.params.id, from:req.params.contact}).populate("from matter to")
+    //     .then(data => {
             // res.status(200).json({status: true, message:" fetched"})
 
 //         }).catch(error => {
