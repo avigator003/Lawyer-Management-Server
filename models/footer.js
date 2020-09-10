8e3c932969640d6770629c4f0a9b02fa762045c9
Anas M.I <anas3rde@gmail.com>
2020-09-10 17:13:52 +0530

    footer model

---
+++ models/footer.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const config = require('../config')
+const schemaOptions = {
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+  };
+const Footer = new Schema({
+    logo:String,
+    description:String,
+    list:Object,
+    list2:Object,
+    address:String,
+    socialMedia:Object
+},schemaOptions)
+
+module.exports = mongoose.model('Footer', Footer)
+
+
+
