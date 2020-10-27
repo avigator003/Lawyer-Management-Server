27a14fa09cb487bb2abf683b6c0091932f748b17
Anas M.I <anas3rde@gmail.com>
2020-10-27 15:18:38 +0530

    logs route

---
+++ routes/api/logs/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router()
+const controller = require('./logs.controller')
+
+
+router.post("/create", controller.createLogs)
+router.get("/delete/:id", controller.deleteLogs)
+router.get("/showall", controller.showAllLogs)
+router.post("/edit/:id", controller.editlogs)
+router.get("/view/:id", controller.viewlogs)
+router.get("/viewforuser/:id", controller.viewSpecific)
+
+
+module.exports = router
