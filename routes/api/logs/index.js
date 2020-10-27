315ca422af1d1970b8b9ca141f3cae59ff97a77d
Anas M.I <anas3rde@gmail.com>
2020-10-27 18:38:54 +0530

    acccount api added to routes

--- routes/api/logs/index.js
+++ routes/api/logs/index.js
@@ -8,6 +8,7 @@ router.get("/showall", controller.showAllLogs)
 router.post("/edit/:id", controller.editlogs)
 router.get("/view/:id", controller.viewlogs)
 router.get("/viewforuser/:id", controller.viewSpecific)
+router.get("/viewforaccount/:id/:account", controller.viewSpecificForAccount)
 
 
 module.exports = router
