7b251916f8c44d5b243c35cacaa20e269b5e016d
Jess70 <jppatidarjay713@gmail.com>
2020-12-02 19:30:29 +0530

    email service

--- routes/api/communication/communication.controller.js
+++ routes/api/communication/communication.controller.js
@@ -1,21 +1,28 @@
 const Communication = require('../../../models/communication')
 const nodemailer = require("nodemailer")
-var request=require('request');
+var request = require('request');
 
 var sesTransport = require('nodemailer-ses-transport');
 
 var SESCREDENTIALS = {
-  accessKeyId : "accesskey" ,
-  secretAccessKey : "secretkey"
+    accessKeyId: "accesskey",
+    secretAccessKey: "secretkey"
 };
+/*
+let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // use SSL
+    auth: {
+      user: "monarchwebapp@gmail.com", // generated ethereal user
+      pass: "MONARCH123", // generated ethereal password
+    },
+  });
+*/
 
 var transporter = nodemailer.createTransport(sesTransport({
-
-    
-    accessKeyId: process.env.accessKeyId,
-    secretAccessKey: process.env.secretAccessKey,
-
- 
+    accessKeyId: "AKIA3OKRZPIAMSX2LH7Q",
+    secretAccessKey: "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",
 }));
 
 // Create New log
@@ -23,25 +30,25 @@ exports.create = (req, res) => {
 
     let communication = new Communication(req.body)
 
- 
+
     communication.save().then(data => {
-            res.status(200).json({status: true, message:"communication Saved", data})
+        res.status(200).json({ status: true, message: "communication Saved", data })
 
-        }).catch(error => {
-        res.status(400).json({status: false, message:error})
+    }).catch(error => {
+        res.status(400).json({ status: false, message: error })
 
-        })
+    })
 }
 
 
 exports.deleteCommunication = (req, res) => {
-// console.log(req.params.id)
-Communication.findByIdAndRemove(req.params.id).
+    // console.log(req.params.id)
+    Communication.findByIdAndRemove(req.params.id).
         then(data => {
-            res.status(200).json({status: true, message:"Communication Removed", data})
+            res.status(200).json({ status: true, message: "Communication Removed", data })
 
         }).catch(error => {
-        res.status(200).json({status: false, message:error})
+            res.status(200).json({ status: false, message: error })
 
         })
 }
@@ -51,10 +58,10 @@ exports.showAll = (req, res) => {
 
     Communication.find({}).populate("matter").
         then(data => {
-            res.status(200).json({status: true, message:"Communication fetched", data})
+            res.status(200).json({ status: true, message: "Communication fetched", data })
 
         }).catch(error => {
-        res.status(200).json({status: false, message:error})
+            res.status(200).json({ status: false, message: error })
 
         })
 }
@@ -64,10 +71,10 @@ exports.view = (req, res) => {
 
     Communication.findById(req.params.id).populate("matter").
         then(data => {
-            res.status(200).json({status: true, message:"Communication fetched", data})
+            res.status(200).json({ status: true, message: "Communication fetched", data })
 
         }).catch(error => {
-        res.status(200).json({status: false, message:error})
+            res.status(200).json({ status: false, message: error })
 
         })
 }
@@ -76,12 +83,12 @@ exports.view = (req, res) => {
 //fetch for one user
 exports.viewSpecific = (req, res) => {
 
-    Communication.find({userId:req.params.id}).populate("from matter to").
+    Communication.find({ userId: req.params.id }).populate("from matter to").
         then(data => {
-            res.status(200).json({status: true, message:"Communication fetched", data})
+            res.status(200).json({ status: true, message: "Communication fetched", data })
 
         }).catch(error => {
-        res.status(200).json({status: false, message:error})
+            res.status(200).json({ status: false, message: error })
 
         })
 }
@@ -89,12 +96,12 @@ exports.viewSpecific = (req, res) => {
 
 exports.viewForMatter = (req, res) => {
 
-    Communication.find({userId:req.params.id, matter:req.params.matter}).populate("from matter to").
+    Communication.find({ userId: req.params.id, matter: req.params.matter }).populate("from matter to").
         then(data => {
-            res.status(200).json({status: true, message:"Communication fetched", data})
+            res.status(200).json({ status: true, message: "Communication fetched", data })
 
         }).catch(error => {
-        res.status(200).json({status: false, message:error})
+            res.status(200).json({ status: false, message: error })
 
         })
 }
@@ -102,32 +109,32 @@ exports.viewForMatter = (req, res) => {
 //Edit contact
 exports.editCommunication = (req, res) => {
 
-    Communication.findByIdAndUpdate(req.params.id, req.body, {new: true}).
+    Communication.findByIdAndUpdate(req.params.id, req.body, { new: true }).
         then(data => {
-            res.status(200).json({status: true, message:"Communication updated", data})
+            res.status(200).json({ status: true, message: "Communication updated", data })
 
         }).catch(error => {
-        res.status(400).json({status: false, message:error})
+            res.status(400).json({ status: false, message: error })
 
         })
 }
 
-   //fetch for one user and client
-   exports.viewSpecificCommunicationClient = (req, res) => {
-    
-    Communication.find({userId:req.params.id, from:req.params.contact}).populate("from matter to")
+//fetch for one user and client
+exports.viewSpecificCommunicationClient = (req, res) => {
+
+    Communication.find({ userId: req.params.id, from: req.params.contact }).populate("from matter to")
         .then(data => {
-            res.status(200).json({status: true, message:" fetched", data})
+            res.status(200).json({ status: true, message: " fetched", data })
 
         }).catch(error => {
-        res.status(200).json({status: false, message:error})
+            res.status(200).json({ status: false, message: error })
 
         })
 }
 
- //fetch for one user and client
- exports.sendEmail = (req, res) => {
-    
+//fetch for one user and client
+exports.sendEmail = (req, res) => {
+
 
     // var transporter = nodemailer.createTransport({
     //     service: 'gmail',
@@ -136,7 +143,7 @@ exports.editCommunication = (req, res) => {
     //       pass: '8123342590'
     //     }
     //   });
-    
+
     //   var mailOptions = {
     //     from: 'FromName <registeredMail@xxx.com>',
     //     to: 'registeredMail@xyz.com', // list of receivers
@@ -144,26 +151,27 @@ exports.editCommunication = (req, res) => {
     //     html: <p>Mail message</p> // html body
     // };
 
-        // var url = "https://distracted-rosalind-719c41.netlify.app" +'/verified/?token='+user._id;
-      
+    // var url = "https://distracted-rosalind-719c41.netlify.app" +'/verified/?token='+user._id;
+
     //   var userEmail = user.emailAddress;
-      var emailText = 'Please click on the link below to verify your Account';
+    var emailText = 'Please click on the link below to verify your Account';
     //   emailText += '<p><a href="'+url+'">click here</a>';
-      var mailOptions = {
-        from: 'Precedent Online <admin@precedentonline.com>',
+    var mailOptions = {
+        from: '"Precedent Online" <admin@precedentonline.com>', // sender address
         to: req.body.to,
         subject: req.body.subject,
-        html: req.body.text
-      };
-      
-      transporter.sendMail(mailOptions, function(error, info){
+        text: req.body.text, // plain text body
+       // html: "<b>Hello world?</b>", // html body
+    };
+    //console.log(mailOptions)
+    transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
-          console.log(error);
-        //   res.json({ 'success': false, 'message': error });
-        } 
-          res.json({ 'success': true, 'message': 'email sent ' })
-        
-      });
+            console.log(error);
+            //   res.json({ 'success': false, 'message': error });
+        }
+        res.json({ 'success': true, 'message': 'email sent ' })
+
+    });
 
 
 
@@ -172,17 +180,17 @@ exports.editCommunication = (req, res) => {
 
     // Communication.find({userId:req.params.id, from:req.params.contact}).populate("from matter to")
     //     .then(data => {
-            // res.status(200).json({status: true, message:" fetched"})
+    // res.status(200).json({status: true, message:" fetched"})
 
-//         }).catch(error => {
-//         res.status(200).json({status: false, message:error})
+    //         }).catch(error => {
+    //         res.status(200).json({status: false, message:error})
 
-//         })
+    //         })
 }
 
 
 exports.sendSms = (req, res) => {
-    
+
 
     // var transporter = nodemailer.createTransport({
     //     service: 'gmail',
@@ -191,7 +199,7 @@ exports.sendSms = (req, res) => {
     //       pass: '8123342590'
     //     }
     //   });
-    
+
     //   var mailOptions = {
     //     from: 'FromName <registeredMail@xxx.com>',
     //     to: 'registeredMail@xyz.com', // list of receivers
@@ -199,8 +207,8 @@ exports.sendSms = (req, res) => {
     //     html: <p>Mail message</p> // html body
     // };
 
-        // var url = "https://distracted-rosalind-719c41.netlify.app" +'/verified/?token='+user._id;
-      
+    // var url = "https://distracted-rosalind-719c41.netlify.app" +'/verified/?token='+user._id;
+
     //   var userEmail = user.emailAddress;
     //   var emailText = 'Please click on the link below to verify your Account';
     // //   emailText += '<p><a href="'+url+'">click here</a>';
@@ -210,15 +218,23 @@ exports.sendSms = (req, res) => {
     //     subject: req.body.subject,
     //     html: req.body.text
     //   };
-      
+
     //   transporter.sendMail(mailOptions, function(error, info){
     //     if (error) {
     //       console.log(error);
     //     //   res.json({ 'success': false, 'message': error });
     //     } 
-        request.get(`https://smsconnect.techgaia.com/jsapi/send?from=MobileEDGE&to=${req.body.mobile}&username=ksap&password=ksa123&content=${req.body.content}`)
-          res.json({ 'success': true, 'message': 'Sms sent ' })
-        
+    request.get(`https://smsconnect.techgaia.com/jsapi/send?from=MobileEDGE&to=${req.body.mobile}&username=ksap&password=ksa123&content=${req.body.content}`)
+    
+    // .on('response', function(response) {
+    //     console.log(response.statusCode) // 200
+    //     console.log(response.headers['content-type']) // 'image/png'
+    //     //console.log(response.data) // 'image/png'
+
+    //   })
+      
+    res.json({ 'success': true, 'message': 'Sms sent ' })
+
     //   });
 
 
@@ -228,10 +244,10 @@ exports.sendSms = (req, res) => {
 
     // Communication.find({userId:req.params.id, from:req.params.contact}).populate("from matter to")
     //     .then(data => {
-            // res.status(200).json({status: true, message:" fetched"})
+    // res.status(200).json({status: true, message:" fetched"})
 
-//         }).catch(error => {
-//         res.status(200).json({status: false, message:error})
+    //         }).catch(error => {
+    //         res.status(200).json({status: false, message:error})
 
-//         })
+    //         })
 }
